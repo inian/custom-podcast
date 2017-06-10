d433a386a7f417d8a045734db715a57fa3774761
Inian Parameshwaran <inian1234@gmail.com>
2017-06-10 17:50:41 +0800

    deploy script updates lambda script too

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,19 @@
+# copy source files
 mkdir -p dist
 cp *.py dist/
-cp -R dependencies/lib/python2.7/site-packages/* dist/
+
+# install deps
+cp prod-requirements.txt dist/
 cd dist
+pip install -r prod-requirements.txt -t .
+
+# create lambda zip file
 zip -r  ../add1.zip *
+
+# upload lambda zip file to z3
+cd ..
+aws s3 cp ./add1.zip s3://custom-podcasts/code.zip
+
+# update the lambda function using s3 URL
+# assumes lambda function already there
+aws lambda update-function-code --function-name add --s3-bucket custom-podcasts --s3-key code.zip
